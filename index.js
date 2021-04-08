0d85b1c464a6f1f56061697d2cbc0f8aed071102
Sonia <serrano_sonia@hotmail.com>
2021-04-08 18:56:00 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const answer = fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+  return answer;
 }
 
 function renderBooks(books) {
+ console.log('lilbros '+books);
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
